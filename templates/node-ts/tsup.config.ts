a359ef3d999f6c84aa64ecd1f4c2c2705797588f
Ivan Ivanov <ivan.h.ivanov99@gmail.com>
2024-11-06 11:40:57 +0000

    project run dev watch mode bug fixes for local and docker

--- templates/node-ts/tsup.config.ts
+++ templates/node-ts/tsup.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "tsup";
+import dotenv from "dotenv";
 
 export default defineConfig({
   clean: true,
@@ -9,4 +10,5 @@ export default defineConfig({
   outDir: "dist",
   splitting: false,
   sourcemap: true,
+  env: dotenv.config().parsed,
 });
