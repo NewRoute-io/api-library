5e1bce086e59fbcb9f9fa6a5f3a8305c729f5263
Ivan Ivanov <ivan.h.ivanov99@gmail.com>
2024-11-07 13:08:08 +0000

    ignore middleware files during test coverage

--- registry/vitest.config.ts
+++ registry/vitest.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
         "repositories/*",
         "schemaValidators/*",
         "./**/router/*",
+        "./**/middleware/*",
         "**/errors/*"
       ],
     },
