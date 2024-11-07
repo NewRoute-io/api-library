bc0c4f06abca3ad5755b3d49936a7ed265b6a91a
Ivan Ivanov <ivan.h.ivanov99@gmail.com>
2024-11-07 13:06:28 +0000

    add email controller tests; cleanup store file s3 tests

--- registry/vitest.config.ts
+++ registry/vitest.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
       JWT_SECRET_KEY: "testSecretKey",
       JWT_ISSUER: "api.library.tests",
       S3_BUCKET_NAME: "mockBucketName",
+      POSTMARK_SERVER_TOKEN: "xxxx-xxxxx-xxxx-xxxxx-xxxxxx"
     },
     alias: {
       "@/": new URL("./", import.meta.url).pathname,
